ca3d2373bc3c2891d4f1b790876f94368bb643c5
gitueser <jewellena78@gmail.com>
2023-11-18 19:57:44 +0100

    added security config

--- src/main/java/sia/tacocloud/web/config/WebConfig.java
+++ src/main/java/sia/tacocloud/web/config/WebConfig.java
@@ -1,4 +1,4 @@
-package sia.tacocloud.web;
+package sia.tacocloud.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
