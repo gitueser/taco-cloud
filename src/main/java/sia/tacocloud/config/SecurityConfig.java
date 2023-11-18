df16c58de5490a0a09c73fe776f734a7e4aa9b97
gitueser <jewellena78@gmail.com>
2023-11-18 22:31:00 +0100

    added: 1) security config 2) registration and login forms

--- src/main/java/sia/tacocloud/config/SecurityConfig.java
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -2,12 +2,17 @@ package sia.tacocloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import sia.tacocloud.entity.User;
 import sia.tacocloud.data.UserRepository;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +31,21 @@ public class SecurityConfig {
 
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(antMatcher("/design")).hasRole("USER")
+                        .requestMatchers(antMatcher("/orders")).hasRole("USER")
+//                        .requestMatchers("/", "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
     }
 }
