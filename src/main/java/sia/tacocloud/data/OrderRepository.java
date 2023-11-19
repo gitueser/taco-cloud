d1181bc51ec3b57c3be47a1b411154bb14615169
gitueser <jewellena78@gmail.com>
2023-11-19 15:02:24 +0100

    added: 1) 'ordersForUser' method for OrderController 2) 'taco.orders.pageSize' property and description for that 3) 'OrderProps' holder

--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -1,7 +1,9 @@
 package sia.tacocloud.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.entity.TacoOrder;
+import sia.tacocloud.entity.User;
 
 import java.util.Date;
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
     List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/sia/tacocloud/web/OrderController.java
+++ src/main/java/sia/tacocloud/web/OrderController.java
@@ -2,8 +2,11 @@ package sia.tacocloud.web;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +24,18 @@ import sia.tacocloud.entity.User;
 @SessionAttributes("tacoOrder")
 public class OrderController {
     private final OrderRepository orderRepo;
+    private final OrderProps orderProps;
 
-    public OrderController(OrderRepository orderRepo) {
+    public OrderController(OrderRepository orderRepo, OrderProps orderProps) {
         this.orderRepo = orderRepo;
+        this.orderProps = orderProps;
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @GetMapping("/current")
