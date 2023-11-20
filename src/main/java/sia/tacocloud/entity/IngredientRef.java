0adff579c6d4ed5b1681c4f7b62fad29a4539530
gitueser <jewellena78@gmail.com>
2023-11-21 00:15:11 +0100

    code refactoring

--- src/main/java/sia/tacocloud/entity/IngredientRef.java
+++ src/main/java/sia/tacocloud/entity/IngredientRef.java
@@ -4,13 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class IngredientRef {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private final String ingredient;
+    private String ingredient;
+
+    public IngredientRef(String ingredientId) {
+        ingredient = ingredientId;
+    }
 }
---
+++ src/main/java/sia/tacocloud/web/TacoController.java
@@ -0,0 +1,48 @@
+package sia.tacocloud.web;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sia.tacocloud.data.TacoRepository;
+import sia.tacocloud.entity.Taco;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tacos")
+//@RequestMapping(params = "/api/tacos", produces = "application/json")
+@CrossOrigin(origins = "http://tacocloud:8080")
+public class TacoController {
+
+    private final TacoRepository tacoRepo;
+
+    public TacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+//    @GetMapping(params = "recent")
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        System.out.println("DASFSDG");
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        Page<Taco> tacos = tacoRepo.findAll(page);
+        return tacos;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        return optTaco
+                .map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+}
