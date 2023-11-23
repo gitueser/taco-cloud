9e2fbb2179ad344c552cf8c37ad90d63a9ecd136
gitueser <jewellena78@gmail.com>
2023-11-23 23:28:48 +0100

    code refactoring

--- src/main/java/sia/tacocloud/config/SecurityConfig.java
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package sia.tacocloud.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
