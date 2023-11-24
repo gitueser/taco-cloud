33073bf1863d02889df9edc0fcd3fcb880e5e7f0
gitueser <jewellena78@gmail.com>
2023-11-24 19:05:10 +0100

    code refactoring

--- src/main/java/sia/tacocloud/TacoCloudApplication.java
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -2,7 +2,6 @@ package sia.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TacoCloudApplication {
